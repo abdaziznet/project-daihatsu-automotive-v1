fc96d71f07f5aa5ee421c391636aeec59e42d82e
Abdul Aziz <dev.abdaziz@outlook.com>
2025-07-22 20:42:59 +0700

    penambahan sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: "https://daihatsuautomotive.com", // Ganti dengan domain Anda
+  generateRobotsTxt: true, // Akan otomatis buat robots.txt juga
+  sitemapSize: 5000,
+};
